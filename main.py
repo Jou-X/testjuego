44223e04c3e23eacf5e65fd1caad27a26507702c
Jou-X <legogogo2019@gmail.com>
2025-01-31 08:01:53 +0100

    nuetras primera actualizacion yipee, le he puesto un def de daño de monstruo, se puede añadir el valor que sea

--- main.py
+++ main.py
@@ -17,22 +17,56 @@ inventario = {
 
 def abrir_inventario():
     
-    item = input(f"Tu inventario es {inventario}. ¿Qué te gustaria usar?")
+    item = input(f"Tu inventario es {inventario}. ¿Qué te gustaria usar?: ")
     
-    for item in inventario:
+    if item in inventario:
         
         if inventario[item] <=0:
-            print(f"No te queda. Tienes {item}")
+            print(f"No te queda. Tienes {inventario[item]}")
+
 
         elif inventario[item] >=1:
             inventario[item] -= 1
-            print(inventario)
+            print(f"Has usado {item}")
+            return(item)
+    else:
+        print("No tienes ese objeto en tu inventario")
            
-        elif item not in inventario:
-            print("No tienes ese item")
-            break
-        
-abrir_inventario()
+def curar():
+
+    global vida
+    cura = abrir_inventario()
+    
+    if cura == "pan":
+        vida += 3
+        print(f"Te has sanado +3. Ahora tienes {vida}")
+        return vida
+    if cura == "golden apple":
+        vida += 50
+        print(f"Te has sanado +50. Ahora tienes {vida}")
+        return vida
+    if cura == "chocolate":
+        vida += 15
+        print(f"Te has sanado +15. Ahora tienes {vida}")
+        return vida
+
+def daño_mob(daño):
+    global vida
+    if daño >=vida:
+        print("Has muerto")
+        return 0
+    
+    vida = vida - daño
+    print(f"Tu vida está a {vida}")
+    return vida
+    
+    
+
+daño_mob(50)
+print(vida)
+vida = curar()
+print(vida)
+
 
 
 
@@ -77,7 +111,7 @@ def poco01(texto):
 
 def poco02(texto):
     for letra in texto:
-        sys.stdunt.write(ltra)
+        sys.stdunt.write(letra)
         sys.stdunt.flush()
         time.sleep(0.06)
     print()
@@ -110,5 +144,5 @@ nombres = random.choice(["Misco Jones","Gilito Mcpato","n+i+g+g+a"])
 
 
 
-poco3("Bienvenido")
-poco2("me llamo", nombres)
+# poco3("Bienvenido")
+# poco2("me llamo", nombres)
