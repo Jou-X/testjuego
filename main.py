338e06aa0b3e8090e26e1849b0824d8dd560f13a
Jou-X <legogogo2019@gmail.com>
2025-02-01 16:12:31 +0100

    actualizacion de inventario (funciona y no se ni como) no cambies nada pls

--- main.py
+++ main.py
@@ -6,36 +6,102 @@ import csv
 import os
 import math
 
+
 vida = 100
+daño = 5
 
 inventario = {
-    "pan" : 1,
-    "chocolate" : 2,
-    "golden apple" : 2,
-    "espada" : 1,
+    
+    "inventario_comida" : {
+        "pan" : 1,
+        "chocolate" : 2,
+        "golden apple" : 2,
+    },
+
+    "inventario_armas" : {
+        "cuchillo" : 1,
+        "espada" : 0,
+    }
 }
 
+
 def abrir_inventario():
-    
-    item = input(f"Tu inventario es {inventario}. ¿Qué te gustaria usar?: ")
-    
-    if item in inventario:
+    print()
+
+    while True:
+
+        tipo_inventario = input("¿Qúe buscas?. A- Comida B- Armas E- Cerrar Inventario: ").lower()
+
+        if tipo_inventario == 'a':
+            curar()
+            break
+        if tipo_inventario == 'b':
+            inventario_armas()
+            break
+        if tipo_inventario == 'e':
+            break
+        else:
+            print("Respuesta no válida.")
+            continue
+
+def inventario_comida():
+    while True:
+            print("Pulsa 'e' cuando quieras salir.")
+            item = input(f"Tu inventario es {inventario['inventario_comida']}. ¿Qué te gustaría usar?: ")
+
+            if item == 'e':
+                print("Saliendo del inventario...")
+                break
+
+            print()
+            if item in inventario['inventario_comida']:
+                
+                if inventario['inventario_comida'][item] <=0:
+                    print(f"No te queda. Tienes {inventario['inventario_comida'][item]}")
+                    return 0
+
+                elif inventario['inventario_comida'][item] >=1:
+                    inventario['inventario_comida'][item] -= 1
+                    print(f"Has usado {item}")
+                    return(item)
+            else:
+                print("No tienes ese objeto en tu inventario")
+                continue
+            break
+
+def inventario_armas():
+    while True:
+        print("Pulsa 'e' cuando quieras salir.")
+        item = input(f"Tu inventario es {inventario['inventario_armas']}. ¿Qué te gustaría equipar?: ")
+
+        if item == 'e':
+            print("Saliendo del inventario...")
+            break
+
+        print()
+        if item in inventario['inventario_armas']:
+                
+            if inventario['inventario_armas'][item] ==0:
+                print(f"Aún no has encontrado eso.")
+                return 0
+
+            elif inventario['inventario_armas'][item] ==1:  
+                print(f"Has equipado {item}")
+                return(item)
+        else:
+            print(f"{item} no existe.")
+            continue
         
-        if inventario[item] <=0:
-            print(f"No te queda. Tienes {inventario[item]}")
+        if item == "e":
+            print("Saliendo del inventario...")
+            break
 
 
-        elif inventario[item] >=1:
-            inventario[item] -= 1
-            print(f"Has usado {item}")
-            return(item)
-    else:
-        print("No tienes ese objeto en tu inventario")
-           
 def curar():
 
     global vida
-    cura = abrir_inventario()
+
+    cura = inventario_comida()
     
     if cura == "pan":
         vida += 3
@@ -50,28 +116,29 @@ def curar():
         print(f"Te has sanado +15. Ahora tienes {vida}")
         return vida
 
-def daño_mob(daño):
-    global vida
-    if daño >=vida:
-        print("Has muerto")
-        return 0
-    
-    vida = vida - daño
-    print(f"Tu vida está a {vida}")
+def vida_p(vida):
+    vida = daño_mob()
     return vida
     
+
+
+def daño_mob(daño_m):
+
+    global vida
+
+    if daño_m >=vida:
+        print("Has muerto")
+        quit()
     
-
-daño_mob(50)
-print(vida)
-vida = curar()
-print(vida)
+    vida = vida - daño_m
+    print(f"Has recibido {daño_m} de daño. Tu vida está a {vida}")
+    return vida
 
 
+daño_mob(2)
 
 
-
-
+abrir_inventario()
 
 
 
